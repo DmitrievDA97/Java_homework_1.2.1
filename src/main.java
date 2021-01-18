9343627863b56d4412a95aa120ebd2b98b16c678
Denis Dmitriev <denitos97@gmail.com>
2021-01-18 12:24:14 +0300

    Added Money Transfer

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = balance + transfer;
+        System.out.println(total);
+    }
+}
